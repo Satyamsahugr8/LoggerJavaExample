077cd0173c9467f4ffacc96a7a88f7ba100b1cb0
satyamsahugr8 <satyamsahugr8@gmail.com>
2023-10-26 20:58:20 +0530

    my first commit

---
+++ src/com/example/MyService.java
@@ -0,0 +1,47 @@
+package com.example;
+
+import org.apache.logging.log4j.Logger;
+
+import com.runner.LoggerConfiguration;
+
+public class MyService {
+	
+	private Logger LOGGER = LoggerConfiguration.Configurator(MyService.class.getName());
+	
+	public void method1() {
+		int a = 1;
+		LOGGER.info("inside method1():: a: [{}]",a);
+	}
+	
+	public void method2() {
+		int b = 2;
+		LOGGER.info("inside method1():: b: [{}]",b);
+	}
+	
+	public void method3() {
+		int c = 3;
+		LOGGER.info("inside method1():: c: [{}]",c);
+	}
+	
+	public void method4() {
+		int d = 4;
+		LOGGER.info("inside method1():: d: [{}]",d);
+	}
+	
+	public void method5() {
+		int e = 5;
+		LOGGER.info("inside method1():: e: [{}]",e);
+	}
+	
+
+	public static void main(String[] args) {
+		
+		MyService myService = new MyService();	
+		myService.method1();
+		myService.method2();
+		myService.method3();
+		myService.method4();
+		myService.method5();	
+		
+	}
+}
